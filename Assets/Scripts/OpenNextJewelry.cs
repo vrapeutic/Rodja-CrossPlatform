34ddeff7ead7a504f9800af49538e1edb6d81791
Andrew Gamal <58505236+Untoucha@users.noreply.github.com>
2022-03-17 14:41:03 +0200

    close #19
    Fix events run will collide with any object problem .

--- Assets/Scripts/OpenNextJewelry.cs
+++ Assets/Scripts/OpenNextJewelry.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class OpenNextJewelry : MonoBehaviour
 {
     [SerializeField]
     List<MeshRenderer> jewelries;
+    [SerializeField]
+    List<XRSimpleInteractable> jewleryInteractor;
+
     int index = 1;
     // Start is called before the first frame update
     void Start()
     {
         jewelries = this.GetComponentsInChildren<MeshRenderer>().ToList();
+        jewleryInteractor = this.GetComponentsInChildren<XRSimpleInteractable>().ToList();
+        for (int i = 0; i < jewleryInteractor.Count; i++)
+        {
+            jewleryInteractor[i + 1].GetComponent<Collider>().enabled = false;
+        }
     }
 
     public void ActivateNextJewelry()
@@ -19,6 +29,7 @@ public class OpenNextJewelry : MonoBehaviour
         if (index < jewelries.Count)
         {
             jewelries[index].enabled = true;
+            jewleryInteractor[index].GetComponent<Collider>().enabled = true;
             index++;
         }
     }
