6670904b0a756ca5a09c07d702a8458d705efc47
Andrew Gamal <58505236+Untoucha@users.noreply.github.com>
2022-03-24 20:22:58 +0200

    Menu Scale Set + Enhance Butterfly Movement

--- Assets/Level 2 destractor/DestractorWayPoint.cs
+++ Assets/Level 2 destractor/DestractorWayPoint.cs
@@ -15,7 +15,7 @@ public class DestractorWayPoint : MonoBehaviour
     int currentIndex;
     bool CanAgentMove;
     float distance;
-    bool canStartCheckDistance =true;
+    bool canStartCheckDistance = true;
 
     void Start()
     {
@@ -28,50 +28,52 @@ public class DestractorWayPoint : MonoBehaviour
 
     private void MoveAgentToNextPoint()
     {
-            CanAgentMove = true;
-           // Debug.Log("move to next point");
+        CanAgentMove = true;
+        // Debug.Log("move to next point");
     }
 
     private void GetNextPointIndex()
     {
-       int nextIndex = Random.Range(0, wayPoints.Count);
+        int nextIndex = Random.Range(0, wayPoints.Count);
         if (nextIndex == currentIndex) GetNextPointIndex();
-        currentIndex = nextIndex;       
+        currentIndex = nextIndex;
     }
-   
+
 
     IEnumerator DestractorArrivedAtPoint()
     {
         GetNextPointIndex();
-      //  Debug.Log("destractor arrived");
+        //  Debug.Log("destractor arrived");
         CanAgentMove = false;
         yield return new WaitForSeconds(2f);
         MoveAgentToNextPoint();
         yield return new WaitForSeconds(3f);
         canStartCheckDistance = true;
     }
-   
+
 
     private void Update()
-    {     
-            Vector3 wayPointPosition = new Vector3(wayPoints[currentIndex].transform.position.x, 2, wayPoints[currentIndex].transform.position.z);
-            distance = Vector3.Distance(level2Destractor.transform.position, wayPointPosition);
-            if (distance < 0.1) StartCoroutine(DestractorArrivedAtPoint());
-    
+    {
+        Vector3 wayPointPosition = new Vector3(wayPoints[currentIndex].transform.position.x, 2, wayPoints[currentIndex].transform.position.z);
+        distance = Vector3.Distance(level2Destractor.transform.position, wayPointPosition);
+        if (distance < 0.1) StartCoroutine(DestractorArrivedAtPoint());
+
     }
 
     private void FixedUpdate()
     {
         if (CanAgentMove)
-        {          
-           level2Destractor.transform.position += GetAgentDirection() * destractorSpeed * Time.fixedDeltaTime;
-            level2Destractor.transform.LookAt(GetAgentDirection());
+        {
+            level2Destractor.transform.position += GetAgentDirection() * destractorSpeed * Time.fixedDeltaTime;
+            //level2Destractor.transform.LookAt(GetAgentDirection());
+            level2Destractor.transform.rotation = Quaternion.Slerp(level2Destractor.transform.rotation, Quaternion.LookRotation(GetAgentDirection()), 0.55f);
+
         }
     }
 
     private Vector3 GetAgentDirection()
     {
-       // Debug.Log("get agent Direction");
+        // Debug.Log("get agent Direction");
         Vector3 dir;
         Vector3 dirNormalized;
         Vector3 wayPointNewPosition;
